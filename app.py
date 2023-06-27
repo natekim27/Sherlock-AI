71fe838c823355d155e82b85f0a1529e747ee0b6
Aheadboat <alex.liang@zoom.us>
2023-06-27 15:07:44 -0700

    api endpoint and token

--- app.py
+++ app.py
@@ -49,12 +49,12 @@ def handle_query():
         return jsonify({'error': 'No query provided'}), 400
 
     # Define the API endpoint
-    endpoint = 'https://chatgptAPI (KEVIN THIS IS ALL YOU)'
+    endpoint = 'https://smartprompt-globaldev.zoomdev.us/v1/zoom-ai-hackathon/invoke'
 
     # Define the headers. Replace 'api-key' with Kevin's OpenAI API key.
     headers = {
         'Content-Type': 'application/json',
-        'Authorization': 'Bearer api-key'
+        'Authorization': 'Bearer eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJpbnRlZ3JhdGlvbi1hZGxlciIsImF1ZCI6InNtYXJ0X3Byb21wdCIsImV4cCI6MTY4ODQ5MDcwMX0.KYfmZ_HuQDd5Yhe0IXpkWWLJqkJ0ZHdjkkYPvWxJhN9fxru7iIRCZqd8BY8UBub7eovWhDxNIucoS1Dd5wj4LQ'
     }
 
     # Define the data. The 'prompt' is the query from the user.
